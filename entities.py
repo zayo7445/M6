be2e4937bce4c9dd6bc8bbfec373a70dff3cc1dd
lokeig <loke_ohrstrom@hotmail.se>
2024-10-21 11:41:30 +0200

    Added player and player movement

--- entities.py
+++ entities.py
@@ -14,7 +14,30 @@ class Entity(pygame.sprite.Sprite):
         self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect(center=self.position)
 
+    def update(self):
+        self.rect = self.position
 
 
+class Enemy(Entity):
+    def init(self, position, width, height, speed, health):
+        super().init(position, width, height, speed, health)
+        self.image.fill((255, 255, 255))
+
+    def create(self):
+        help
 
 
+class Player(Entity):
+    def init(self, position, width, height, speed, health):
+        super().init(position, width, height, speed, health)
+        self.image.fill((255, 255, 255))
+
+    def move(self, keys):
+        if keys[pygame.K_w] and self.position.y > 0:
+            self.position.y -= self.speed
+        if keys[pygame.K_s] and self.position.y < 1080 - self.height:
+            self.position.y += self.speed
+        if keys[pygame.K_d] and self.position.x < 1920 - self.width:
+            self.position.x += self.speed
+        if keys[pygame.K_a] and self.position.x > 0:
+            self.position.x -= self.speed
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import particles
 import pygame
-
-
+from entities import *
 pygame.init()
 
 
 def main():
-    window = pygame.display.set_mode((2560, 1440))
+    window = pygame.display.set_mode((1920, 1080))
     pygame.display.set_caption("Space Invaders")
 
     stars = pygame.sprite.Group(particles.Star(window) for _ in range(100))
 
+    player = Player(position=(100, 100), width=100, height=50, speed=5, health=150)
+
     enemies = pygame.sprite.Group()
+    players = pygame.sprite.Group(player)
     projectiles = pygame.sprite.Group()
     explosions = pygame.sprite.Group()
 
@@ -23,16 +25,25 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pos_x = player.position.x + player.width
+                    pos_y = player.position.y + player.height/2
+                    projectiles.add(particles.Projectile(window, (pos_x, pos_y)))
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     projectiles.add(particles.Projectile(window, event.pos))
                 elif event.button == 3:
                     explosions.add(particles.Explosion(100, event.pos) for _ in range(50))
 
+        keys = pygame.key.get_pressed()
+        player.move(keys)
 
         stars.update()
         stars.draw(window)
+
+        players.draw(window)
+        players.update()
 
         projectiles.update()
         projectiles.draw(window)
